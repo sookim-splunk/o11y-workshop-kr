f2238e0ab2ab33793915cb59ed796a90e97f6c6c
egs125 <egs125@gmail.com>
2025-03-09 15:42:27 +0900

    test

---
+++ src/js/common.js
@@ -0,0 +1,13 @@
+(function() {
+  console.debug('load common.js');
+
+  var _com = () =>  {
+
+
+    return {
+      loadSideBar: () => {
+        console.log('loadSideBar');
+      },
+    };
+  };
+})();
---
+++ src/js/includes.js
@@ -0,0 +1,15 @@
+'use strict';
+
+document.write(`
+  <link href="/workshop/src/images/favicon.ico" rel="icon" type="image/x-icon" sizes="any">
+  <link href="/workshop/src/css/fontawesome-all.min.css" rel="stylesheet" media="print" onload='this.media="all",this.onload=null'>
+  <link href="/workshop/src/css/perfect-scrollbar.min.css" rel="stylesheet">
+  <link href="/workshop/src/css/theme.min.css" rel="stylesheet">
+  <link href="/workshop/src/css/format-html.min.css" rel="stylesheet" id="R-format-style">
+
+  <script src="/workshop/src/js/libs/perfect-scrollbar.min.js" defer></script>
+  <script src="/workshop/src/js/libs/theme.js" defer></script>
+  <script src="/workshop/src/js/libs/clipboard.min.js" defer></script>
+
+  <script src="/workshop/src/js/common.js" defer></script>
+`);
