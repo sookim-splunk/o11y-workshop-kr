4333d227678c5ca7aa0150b4aece981d632fe98f
egs125 <egs125@gmail.com>
2025-03-09 16:57:36 +0900

    test

--- src/js/common.js
+++ src/js/common.js
@@ -2,20 +2,11 @@ var _com = (function() {
   console.debug('load common.js');
 
   const setPage = pageId => {
-
     loadTopBar(pageId);
     loadSideBar(pageId);
   };
 
-  const loadTopBar =  pageId => {
-    fetch('/workshop/src/layout/sidebar.html')
-      .then(res => res.text())
-      .then(html => document.querySelector('#R-sidebar').innerHTML = html)
-      .catch(error => console.error('Failed to fetch page: ', error));
-  };
-
-  const loadSideBar = pageId => {
-    console.log('loadSideBar');
+  const loadTopBar = pageId => {
     fetch('/workshop/src/layout/topbar.html')
       .then(res => res.text())
       .then(html => {
@@ -71,6 +62,62 @@ var _com = (function() {
       .catch(error => console.error('Failed to fetch page: ', error));
   };
 
+  const loadSideBar =  pageId => {
+    fetch('/workshop/src/layout/sidebar.html')
+      .then(res => res.text())
+      .then(html => {
+        document.querySelector('#R-sidebar').innerHTML = html;
+        // document.querySelector('#menu-list')
+        let sideHTML = '';
+        sideHTML = ('', sideHTML, CONTENTS);
+        console.log('2342');
+      })
+      .catch(error => console.error('Failed to fetch page: ', error));
+  };
+
+  const createMenuHTML = (parentId = '', htmlStr, list) => {
+    if ( parentId ) {
+      htmlStr += `<ul id="side-section-${ parentId }" class="collapsible-menu">`;
+    }
+    
+    list.forEach(({
+      id,
+      depth,
+      parentId = '',
+      menuName,
+      href,
+      sub = [],
+    }) => {
+      htmlStr += `
+        <li data-nav-id="/workshop/src${ href }">
+          <input
+            type="checkbox"
+            id="side-section-${ id }"
+            aria-controls="side${ sub.length > 0 ? `-subsections-${ sub[0].id }0` : `-` }"
+          >
+          <label for=side-section-${ id }">
+            <i class="fa-fw fas fa-chevron-right"></i>
+            <span class="a11y-only">Submenu ${ menuName }</span>
+          </label>
+          <a class="padding" href="/workshop/src${ href }">
+            ${ menuName }
+            <i class="fa-fw fas fa-check read-icon"></i>
+          </a>
+      `;
+
+      if ( sub.length > 0 ) {
+        htmlStr += createMenuHTML(parentId, htmlStr, sub);
+      }
+
+      htmlStr += '</li>';
+    });
+    
+    if ( parentId ) {
+      htmlStr += `</ul>`;
+    }
+
+    return htmlStr;
+  };
 
   return {
     setPage: pageId => setPage(pageId),
@@ -78,8 +125,6 @@ var _com = (function() {
   };
 })();
 
-// Set Global Variables
-window.T_Copy_to_clipboard = `Copy to clipboard`; 
 window.T_Copied_to_clipboard = `Copied to clipboard!`; 
 window.T_Copy_link_to_clipboard = `Copy link to clipboard`;
 window.T_Link_copied_to_clipboard = `Copied link to clipboard!`; 
--- src/js/contentList.js
+++ src/js/contentList.js
@@ -1,18 +1,23 @@
 var CONTENTS = [
   {
     id: '1',
+    depth: 0,
     menuName: 'ITSI4Rookies🐥',
     title: 'ITSI4Rookies🐥',
     href: '/ITSI4Rookies/index.html',
     sub: [
       {
         id: '1-1',
+        depth: 1,
+        parentId: '1',
         menuName: 'ITSI4Rookies🐥',
         title: 'Optimize Cloud Monitoring',
         href: '/ITSI4Rookies/optimize-cloud-monitoring/test.html',
         sub: [
           {
             id: '1-1-1',
+            depth: 2,
+            parentId: '1-1',
             title: '1. Getting Started',
             href: '/ITSI4Rookies/optimize-cloud-monitoring/1-getting-started/test2.html',
           }
@@ -22,12 +27,13 @@ var CONTENTS = [
   },
   {
     id: '2',
-    menuName: 'Ninja Workshops',
-    title: `
+    depth: 0, 
+    menuName: `
       Ninja Workshops
       <i class="fa fa-user-ninja"></i>
       <i class="fa-fw fas fa-check read-icon"></i>
     `,
+    title: `Ninja Workshops`,
     href: '/ninja-workshop/index.html',
   }
 
