e8cfe29d87e2fb7dd11ca14277d5c7ba6fad4b1d
sookim-splunk <160687383+sookim-splunk@users.noreply.github.com>
2025-03-11 13:24:01 +0900

    Update includes.js

--- src/js/includes.js
+++ src/js/includes.js
@@ -1,16 +1,16 @@
 'use strict';
 
 document.write(`
-  <link href="src/images/favicon.ico" rel="icon" type="image/x-icon" sizes="any">
-  <link href="src/css/fontawesome-all.min.css" rel="stylesheet" media="print" onload='this.media="all",this.onload=null'>
-  <link href="src/css/perfect-scrollbar.min.css" rel="stylesheet">
-  <link href="src/css/theme.min.css" rel="stylesheet">
-  <link href="src/css/format-html.min.css" rel="stylesheet" id="R-format-style">
+  <link href="./src/images/favicon.ico" rel="icon" type="image/x-icon" sizes="any">
+  <link href="./src/css/fontawesome-all.min.css" rel="stylesheet" media="print" onload='this.media="all",this.onload=null'>
+  <link href="./src/css/perfect-scrollbar.min.css" rel="stylesheet">
+  <link href="./src/css/theme.min.css" rel="stylesheet">
+  <link href="./src/css/format-html.min.css" rel="stylesheet" id="R-format-style">
 
-  <script src="src/js/libs/perfect-scrollbar.min.js" defer></script>
-  <script src="src/js/libs/theme.js" defer></script>
-  <script src="src/js/libs/clipboard.min.js" defer></script>
+  <script src="./src/js/libs/perfect-scrollbar.min.js" defer></script>
+  <script src="./src/js/libs/theme.js" defer></script>
+  <script src="./src/js/libs/clipboard.min.js" defer></script>
 
-  <script src="src/js/contentList.js"></script>
-  <script src="src/js/common.js"></script>
+  <script src="./src/js/contentList.js"></script>
+  <script src="./src/js/common.js"></script>
 `);
