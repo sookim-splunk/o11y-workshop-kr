b33f72745c199600aeec909ce4f29350217040b4
egs125 <egs125@gmail.com>
2025-03-11 20:30:29 +0900

    edit paths

--- src/js/contentList.js
+++ src/js/contentList.js
@@ -3,7 +3,7 @@ var CONTENTS = [
   {
     id: '0',                                  // required. page ID
     title: 'Splunk Observability Workshops',  // required. Bread crumbs text
-    href: './src/index.html',         // required. HTML file path
+    href: '/o11y-workshop-kr/src/index.html',         // required. HTML file path
     prev: '',
     next: '1',
     children: [
@@ -11,7 +11,7 @@ var CONTENTS = [
         id: '1',
         parentId: '0',
         title: 'ITSI4Rookies🐥',
-        href: './src/ITSI4Rookies/index.html',
+        href: '/o11y-workshop-kr/src/ITSI4Rookies/index.html',
         prev: '1',
         next: '1-1',
         children: [
@@ -19,14 +19,14 @@ var CONTENTS = [
             id: '1-1',
             parentId: '1',
             title: 'Optimize Cloud Monitoring',
-            href: './src/ITSI4Rookies/optimize-cloud-monitoring/test.html',
+            href: '/o11y-workshop-kr/src/ITSI4Rookies/optimize-cloud-monitoring/test.html',
             children: [
               {
                 id: '1-1-1',
                 parentId: '1-1',
                 menuName: '1. Getting Started',
                 title: 'Getting Started',
-                href: './src/ITSI4Rookies/optimize-cloud-monitoring/1-getting-started/test2.html',
+                href: '/o11y-workshop-kr/src/ITSI4Rookies/optimize-cloud-monitoring/1-getting-started/test2.html',
               }
             ],
           },
@@ -36,7 +36,7 @@ var CONTENTS = [
         id: '2',
         parentId: '0',
         title: `Ninja Workshops`,
-        href: './src/ninja-workshop/index.html',
+        href: '/o11y-workshop-kr/src/ninja-workshop/index.html',
       }
     ],
   },
