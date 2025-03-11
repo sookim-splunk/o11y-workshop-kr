c6e7cf4928a8d663138f5ccfce8d7062a16bdbfc
sookim-splunk <160687383+sookim-splunk@users.noreply.github.com>
2025-03-11 13:01:27 +0900

    Update includes.js

--- src/js/includes.js
+++ src/js/includes.js
@@ -1,16 +1,16 @@
 'use strict';
 
 document.write(`
-  <link href="/workshop/src/images/favicon.ico" rel="icon" type="image/x-icon" sizes="any">
-  <link href="/workshop/src/css/fontawesome-all.min.css" rel="stylesheet" media="print" onload='this.media="all",this.onload=null'>
-  <link href="/workshop/src/css/perfect-scrollbar.min.css" rel="stylesheet">
-  <link href="/workshop/src/css/theme.min.css" rel="stylesheet">
-  <link href="/workshop/src/css/format-html.min.css" rel="stylesheet" id="R-format-style">
+  <link href="/o11y-workshop-kr/src/images/favicon.ico" rel="icon" type="image/x-icon" sizes="any">
+  <link href="/o11y-workshop-kr/src/css/fontawesome-all.min.css" rel="stylesheet" media="print" onload='this.media="all",this.onload=null'>
+  <link href="/o11y-workshop-kr/src/css/perfect-scrollbar.min.css" rel="stylesheet">
+  <link href="/o11y-workshop-kr/src/css/theme.min.css" rel="stylesheet">
+  <link href="/o11y-workshop-kr/src/css/format-html.min.css" rel="stylesheet" id="R-format-style">
 
-  <script src="/workshop/src/js/libs/perfect-scrollbar.min.js" defer></script>
-  <script src="/workshop/src/js/libs/theme.js" defer></script>
-  <script src="/workshop/src/js/libs/clipboard.min.js" defer></script>
+  <script src="/o11y-workshop-kr/src/js/libs/perfect-scrollbar.min.js" defer></script>
+  <script src="/o11y-workshop-kr/src/js/libs/theme.js" defer></script>
+  <script src="/o11y-workshop-kr/src/js/libs/clipboard.min.js" defer></script>
 
-  <script src="/workshop/src/js/contentList.js"></script>
-  <script src="/workshop/src/js/common.js"></script>
+  <script src="/o11y-workshop-kr/src/js/contentList.js"></script>
+  <script src="/o11y-workshop-kr/src/js/common.js"></script>
 `);
