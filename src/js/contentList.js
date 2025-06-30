d30266a9a3c3052688ccd7bf6107854d171a4141
sookim-splunk <160687383+sookim-splunk@users.noreply.github.com>
2025-06-30 17:31:26 +0900

    correction

--- src/js/contentList.js
+++ src/js/contentList.js
@@ -170,7 +170,7 @@ var CONTENTS = {
               id: '2-1-1', // required. page ID
               title: '2-1-1. Services', // required. Bread crumbs text
               menuName: '2-1-1. Services', // required. Side bar text
-              href: '/o11y-workshop-kr/src/2-Splunk4NinjasITSI/2-1-service-insights/2-1-1-services/2-1-1-index.html', // required. HTML file path
+              href: '/o11y-workshop-kr/src/2-Splunk4NinjasITSI/2-1-service-insights/2-1-1-Services/2-1-1-index.html', // required. HTML file path
               prev: '2-1',
               next: '2-1-2',
             },
