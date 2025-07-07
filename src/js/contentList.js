d9b89e56402b4ad0ae13ff9f610f4c1f230e7f66
sookim-splunk <160687383+sookim-splunk@users.noreply.github.com>
2025-07-07 09:12:04 +0900

    update

--- src/js/contentList.js
+++ src/js/contentList.js
@@ -202,7 +202,7 @@ var CONTENTS = {
               id: '2-1-5', // required. page ID
               title: '2-1-5. Entities', // required. Bread crumbs text
               menuName: '2-1-5. Entities', // required. Side bar text
-              href: '/o11y-workshop-kr/src/2-Splunk4NinjasITSI/2-1-service-insights/2-1-5. Entities/2-1-5-index.html', // required. HTML file path
+              href: '/o11y-workshop-kr/src/2-Splunk4NinjasITSI/2-1-service-insights/2-1-5-Entities/2-1-5-index.html', // required. HTML file path
               prev: '2-1-4',
               next: '2-2',
             },
