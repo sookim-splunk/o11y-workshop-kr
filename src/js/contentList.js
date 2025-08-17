3ed07a8ca3f792bfe70374b663e9c8643dc14f86
sookim-splunk <160687383+sookim-splunk@users.noreply.github.com>
2025-08-17 14:23:44 +0900

    fix

--- src/js/contentList.js
+++ src/js/contentList.js
@@ -217,7 +217,7 @@ var CONTENTS = [
             id: '2-2', // required. page ID
             title: '2-2. Event Analytics', // required. Bread crumbs text
             menuName: '2-2. Event Analytics', // required. Side bar text
-            href: '/o11y-workshop-kr/src/2-Splunk4NinjasITSI/2-2. Event Analytics/2-2-index.html', // required. HTML file path
+            href: '/o11y-workshop-kr/src/2-Splunk4NinjasITSI/2-2-Event-Analytics/2-2-index.html', // required. HTML file path
             prev: '2-1-4',
             next: '2-2-1',
             sub: [
@@ -225,7 +225,7 @@ var CONTENTS = [
                 id: '2-2-1', // required. page ID
                 title: '2-2-1. Thresholds and Time Policies', // required. Bread crumbs text
                 menuName: '2-2-1. Thresholds and Time Policies', // required. Side bar text
-                href: '/o11y-workshop-kr/src/2-Splunk4NinjasITSI/2-2. Event Analytics/2-2-1-Thresholds/2-2-1-index.html', // required. HTML file path
+                href: '/o11y-workshop-kr/src/2-Splunk4NinjasITSI/2-2-Event-Analytics/2-2-1-Thresholds/2-2-1-index.html', // required. HTML file path
                 prev: '2-2',
                 next: '2-2-2',
               },
